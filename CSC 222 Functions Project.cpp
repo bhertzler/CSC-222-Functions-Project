588c4436dc0ffb783e2a4d5b49931e47c4c70fbb
Ben Hertzler <bwhertzler@outlook.com>
2024-12-07 02:18:15 -0500

    Add project files.

---
+++ CSC 222 Functions Project.cpp
@@ -0,0 +1,16 @@
+// CSC 222 Functions Project.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+// Benjamin Hertzler
+// CSC 222
+// Functions Project
+// Master
+//
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
